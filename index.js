7c9672be0d7094726bb1757cc427f828aaa2fa31
Amro dandashli <amrdandashli@gmail.com>
2021-04-14 03:32:06 +0000

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,33 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+const Time24 = (time12h) => {
+  const [time, modifier] = time12h.split(' ');
 
+  let [hours, minutes] = time.split(':');
+
+  if (hours === '12') {
+    hours = '00';
+  }
+
+  if (modifier === 'PM') {
+    hours = parseInt(hours, 10) + 12;
+  }
+
+  return `${hours}:${minutes}`;
+}
+greet(Time24) {
+
+  if (time<12:00pm){let timeless = prompt("Good Morning");
+  prompt( document.getElementById("greeting").write =${timeless};
+    )
+  }
+    else if (time>=5:00 | time <=12:00) {
+      return  "Good Afternoon";
+    }
+ else {
+   return "Good Evening";
+ }
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
