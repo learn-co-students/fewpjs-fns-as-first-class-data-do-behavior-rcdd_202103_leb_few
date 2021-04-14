b88e556245f55d25c2fb08635596c1de936ed1e5
Amro dandashli <amrdandashli@gmail.com>
2021-04-14 11:07:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
-/* Given Code, don't edit */
-
 document.querySelector("button").addEventListener("click", handleClick);
-
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
 }
-
 /* End Given Code, don't edit above here...*/
-const Time24 = (time12h) => {
-  const [time, modifier] = time12h.split(' ');
-
-  let [hours, minutes] = time.split(':');
-
-  if (hours === '12') {
-    hours = '00';
-  }
-
-  if (modifier === 'PM') {
-    hours = parseInt(hours, 10) + 12;
-  }
-
-  return `${hours}:${minutes}`;
-}
-greet(Time24) {
-
-  if (time<12:00pm){let timeless = prompt("Good Morning");
-  prompt( document.getElementById("greeting").write =${timeless};
-    )
-  }
-    else if (time>=5:00 | time <=12:00) {
-      return  "Good Afternoon";
-    }
- else {
-   return "Good Evening";
- }
-}
 /* Write your implementation of greet() */
+function greet(timeString){
+  let th=timeString.split(":")[0];
+  if (th<12){
+    return "Good Morning";
+  }
+  else if (th>12 && th<17){
+    return "Good Afternoon";
+  }
+  else{
+      return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+ let testContent = "TEST";
+function displayMessage(testContent){
+  document.getElementById("greeting").innerText = testContent;
+}
+displayMessage(greet());
