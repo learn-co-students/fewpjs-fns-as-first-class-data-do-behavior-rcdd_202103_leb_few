39501b3691a3e18f017e9b7c686f412b4e8c5020
Charbel Hbayter <charbel.hbayter@lau.edu>
2021-04-14 10:14:50 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(x){
+  let o=x.split(":");
+  let hours = parseInt(o[0],10);
+   if(hours < 12){
+     return "Good Morning";
+   }else if(hours>= 12 && hours <= 17){
+     return "Good Afternoon";
+   }else if(hours > 17){
+       return "Good Evening";
+   }
+  
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(x){
+  let update = document.getElementById("greeting").innerText = x;
+}
+
+
+
+
