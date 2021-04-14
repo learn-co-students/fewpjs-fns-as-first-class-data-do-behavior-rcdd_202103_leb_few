db9a0e4127c20136910a4e847c22e32386cd719c
karen chehade <karen_chehade@hotmail.com>
2021-04-14 09:05:09 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  time.split(":");
+  const hours = parseInt(time);
+
+    if(hours < "12"){
+      return "Good Morning";
+    }else if(hours>"12" && hours < "17"){
+      return "Good Afternoon";
+    }else if (hours > "17") {
+      return "Good Evening";
+    }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message){
+  let greeting = document.getElementById("greeting");
+  greeting.innerText = message;
+}
