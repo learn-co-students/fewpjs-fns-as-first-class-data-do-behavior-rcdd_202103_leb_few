d7dbbf13c38fd19a75f8285308e18beb10011129
Nour El Houda Sarmout <nourelhoudasarmout@gmail.com>
2021-04-14 04:00:05 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  let message;
+  let numTime = time.split(':');
+  numTime = parseInt(numTime);
+
+  if(numTime < 12){
+    message = "Good Morning";
+  }else if(numTime >= 12 && numTime <= 17){
+    message = "Good Afternoon";
+  } else if (numTime > 17){
+    message = "Good Evening";
+  }
+
+  return message;
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message){
+  document.getElementById("greeting").innerText = message;
+}
