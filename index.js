b74bf40b22fd2a930a2edec4ff2728a03aca75b0
Mira Chami <miramc149@gmail.com>
2021-04-17 11:59:13 +0000

    Done.

--- index.js
+++ index.js
@@ -12,19 +12,18 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 function greet(timeString){
 
-    const splits = timeString.split(0, str.indexOf(','));
-    let parsed = parseInt(splits, 2);
-    for(let i=0;i<parsed.length;i++){
-    if(parsed[i] < 12)
+    const splits = timeString.split(":");
+    let parsed = parseInt(splits[0], 10);
+
+    if(parsed < 12)
       return "Good Morning";
-      else if(12<parsed[i]<17)
+      else if(12<parsed<17)
            return"Good Afternoon";
            else
            return "Good Evening";
-  }
+
 }
 /* Write your implementation of displayMessage() */
 function displayMessage(timeString){
-  let content = document.getElementById("greeting").innerText;
-  console.log(element)
+  let content = document.getElementById("greeting").innerText = (timeString);
 }
